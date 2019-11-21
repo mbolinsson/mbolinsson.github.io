4c20ae7acb4168c6131dde294cb14a8d552511b3
Marcus Bolinsson <marcusbolinsson@users.noreply.github.com>
2019-11-21 19:10:50 +0100

    hej

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('hej');
+
+function forever() {
+
+  while (2 < 1) {
+    console.log('forever-and-ever');
+
+  }
+
+  while (2 === 52) {
+    console.log('forever-and-ever');
+
+  }
+
+};
