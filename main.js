dc28d9c70ca4d337d147a8c39a4447a31056e825
Marcus Bolinsson <marcusbolinsson@users.noreply.github.com>
2020-02-18 15:09:08 +0100

    trevligt

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-console.log('hej');
+let hettan = document.querySelector("#hettan");
+let oneMore = 0;
 
+const words = [
+  "Trevligt!",
+  "kul!",
+  "roligt!",
+  "mysigt!",
+  "angenämt!",
+  "underbart!",
+  "fantastiskt!"
+];
 
-function forever() {
-  
-    document.body.style.backgroundColor = "red", 1000000, 
-    document.body.style.backgroundColor = "blue"; 
+// const color = ["red", "green", "black", "tomato", "yellow", "orange", "blue"];
 
-    
-    console.log('forever-and-ever');
-    
+function extraTrevlig() {
+  if (oneMore > 6) {
+    oneMore = 0;
+  }
+  hettan.innerHTML = words[oneMore];
+  //   hettan.style.color = color[oneMore];
+  hettan.style.opacity = "1";
+  oneMore++;
+  console.log(oneMore);
+  window.setTimeout(wait, 1000);
+}
 
-  
+function wait() {
+  hettan.style.opacity = "0";
+  window.setTimeout(extraTrevlig, 1000);
+}
 
-};
+extraTrevlig();
