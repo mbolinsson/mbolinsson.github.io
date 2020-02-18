d105c9f2625109e195269a43013f6a2ed253a206
Marcus Bolinsson <marcusbolinsson@users.noreply.github.com>
2020-02-18 12:24:26 +0100

    hej

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 console.log('hej');
 
-function forever() {
-  while (5 < 10) {
-    console.log('forever-and-ever');
 
-  }
+function forever() {
+  
+    document.body.style.backgroundColor = "red", 1000000, 
+    document.body.style.backgroundColor = "blue"; 
+
+    
+    console.log('forever-and-ever');
+    
+
+  
 
 };
