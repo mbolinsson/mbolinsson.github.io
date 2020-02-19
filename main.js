7cf37041ca28467768b1a81f562beed79f039f7f
Marcus Bolinsson <marcusbolinsson@users.noreply.github.com>
2020-02-19 01:08:44 +0100

    hej

--- main.js
+++ main.js
@@ -1,58 +1,30 @@
-let hettan = document.querySelector(".hettan");
-let oneMore = 0;
-
-const words = [
-  "Hej!",
-  "Bonjour!",
-  "Hallo!",
-  "Hello!",
-  "Ciao!",
-  "Cześć!",
-  "Hola!"
-];
-
-// const color = ["red", "green", "black", "tomato", "yellow", "orange", "blue"];
-
-function extraTrevlig() {
-  if (oneMore > 6) {
-    oneMore = 0;
-  }
-  hettan.innerHTML = words[oneMore];
-  //   hettan.style.color = color[oneMore];
-  hettan.style.opacity = "1";
-  oneMore++;
-  console.log(oneMore);
-  window.setTimeout(wait, 2500);
-}
-
-function wait() {
-  hettan.style.opacity = "0";
-  window.setTimeout(extraTrevlig, 2500);
-}
-
-extraTrevlig();
-
 const ant2 = document.querySelector(".ant2");
+const ant1 = document.querySelector(".ant1");
 
 myror();
 
 function myror() {
   ant2.src = "./ant2.svg";
+  ant1.src = "./ant1.2.svg";
 
   window.setTimeout(myror2, 100);
 }
 
 function myror2() {
   ant2.src = "./ant2.2.svg";
+  ant1.src = "./ant1.3.svg";
   window.setTimeout(myror3, 100);
 }
 
 function myror3() {
   ant2.src = "./ant2.3.svg";
+  ant1.src = "./ant1.11.svg";
   window.setTimeout(myror4, 300);
 }
 
 function myror4() {
   ant2.src = "./ant2.2.svg";
+  ant1.src = "./ant1.12.svg";
+
   window.setTimeout(myror, 100);
 }
