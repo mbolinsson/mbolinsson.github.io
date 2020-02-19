f951ef2533c0bbb3bc3be06cea779e112fd2793f
Marcus Bolinsson <marcusbolinsson@users.noreply.github.com>
2020-02-19 13:06:19 +0100

    hej

--- main.js
+++ main.js
@@ -1,30 +1,34 @@
-const ant2 = document.querySelector(".ant2");
 const ant1 = document.querySelector(".ant1");
+const ant2 = document.querySelector(".ant2");
+const ant3 = document.querySelector(".ant3");
 
 myror();
 
 function myror() {
-  ant2.src = "./ant2.svg";
   ant1.src = "./ant1.2.svg";
+  ant2.src = "./ant2.svg";
+  ant3.src = "./ant3.svg";
 
   window.setTimeout(myror2, 100);
 }
 
 function myror2() {
-  ant2.src = "./ant2.2.svg";
   ant1.src = "./ant1.3.svg";
+  ant2.src = "./ant2.2.svg";
+  ant3.src = "./ant3.2.svg";
   window.setTimeout(myror3, 100);
 }
 
 function myror3() {
-  ant2.src = "./ant2.3.svg";
   ant1.src = "./ant1.11.svg";
-  window.setTimeout(myror4, 300);
+  ant2.src = "./ant2.3.svg";
+  ant3.src = "./ant3.3.svg";
+  window.setTimeout(myror4, 200);
 }
 
 function myror4() {
-  ant2.src = "./ant2.2.svg";
   ant1.src = "./ant1.12.svg";
-
+  ant2.src = "./ant2.2.svg";
+  ant3.src = "./ant3.2.svg";
   window.setTimeout(myror, 100);
 }
