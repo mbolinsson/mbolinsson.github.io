603906db4aaa6a77d928ad4c4de9870124268400
Marcus Bolinsson <marcusbolinsson@users.noreply.github.com>
2020-03-02 01:01:44 +0100

    new stuff

--- main.js
+++ main.js
@@ -1,34 +1,71 @@
 const ant1 = document.querySelector(".ant1");
 const ant2 = document.querySelector(".ant2");
 const ant3 = document.querySelector(".ant3");
+let hide = false;
 
 myror();
+blackhole1();
 
 function myror() {
-  ant1.src = "./ant1.2.svg";
-  ant2.src = "./ant2.svg";
-  ant3.src = "./ant3.svg";
+  ant1.src = "./images/ant1.2.svg";
+  ant2.src = "./images/ant2.svg";
 
-  window.setTimeout(myror2, 100);
+  window.setTimeout(myror2, 50);
 }
 
 function myror2() {
-  ant1.src = "./ant1.3.svg";
-  ant2.src = "./ant2.2.svg";
-  ant3.src = "./ant3.2.svg";
-  window.setTimeout(myror3, 100);
+  ant1.src = "./images/ant1.3.svg";
+  ant2.src = "./images/ant2.2.svg";
+
+  hide = false;
+  window.setTimeout(myror3, 90);
 }
 
 function myror3() {
-  ant1.src = "./ant1.11.svg";
-  ant2.src = "./ant2.3.svg";
-  ant3.src = "./ant3.3.svg";
-  window.setTimeout(myror4, 200);
+  ant1.src = "./images/ant1.11.svg";
+  ant2.src = "./images/ant2.3.svg";
+
+  window.setTimeout(myror4, 100);
 }
 
 function myror4() {
-  ant1.src = "./ant1.12.svg";
-  ant2.src = "./ant2.2.svg";
-  ant3.src = "./ant3.2.svg";
-  window.setTimeout(myror, 100);
+  ant1.src = "./images/ant1.12.svg";
+  ant2.src = "./images/ant2.2.svg";
+
+  window.setTimeout(myror, 70);
+}
+
+// Scared Ant
+
+ant3.addEventListener("mouseenter", scaredAnt);
+
+function scaredAnt() {
+  hide = true;
+}
+
+function blackhole1() {
+  if (hide) {
+    ant3.src = "./images/ant3run.svg";
+    return window.setTimeout(hidingant, 50);
+  }
+  ant3.src = "./images/ant3.svg";
+  window.setTimeout(blackhole2, 50);
+}
+
+function blackhole2() {
+  ant3.src = "./images/ant3.2.svg";
+  window.setTimeout(blackhole3, 30);
+}
+function blackhole3() {
+  ant3.src = "./images/ant3.3.svg";
+  window.setTimeout(blackhole4, 90);
+}
+function blackhole4() {
+  ant3.src = "./images/ant3.2.svg";
+  window.setTimeout(blackhole1, 50);
+}
+
+function hidingant() {
+  ant3.src = "./images/ant3gone.svg";
+  window.setTimeout(blackhole1, 3500);
 }
